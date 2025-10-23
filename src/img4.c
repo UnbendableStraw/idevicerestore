314862423112da680d2739870b51b77bbdee874d
straw <nicsplatts@gmail.com>
2025-10-23 12:26:11 -0700

    Revert "img4: Add RestoreDCP2 -> rdc2 mapping"
    This reverts commit 2914bf66cfb28c6d0fde43655dac05ea044d2907.

--- src/img4.c
+++ src/img4.c
@@ -356,7 +356,6 @@ static const char *_img4_get_component_tag(const char *compname)
 		{ "RecoveryMode", "recm" },
 		{ "RestoreANS", "rans" },
 		{ "RestoreDCP", "rdcp" },
-		{ "RestoreDCP2", "rdc2", },
 		{ "RestoreDeviceTree", "rdtr" },
 		{ "RestoreExtDCP", "recp" },
 		{ "RestoreKernelCache", "rkrn" },
